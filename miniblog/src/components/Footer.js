b88cdc9e027914174ff738477e5f8adc300c9172
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-18 19:05:13 -0300

    atualização

---
+++ miniblog/src/components/Footer.js
@@ -0,0 +1,13 @@
+/* CSS componetizado */
+import styles from "../components/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <h3>Escreva sobre o que você tem interesse!</h3>
+        <p>Mini Blog &copy; 2024</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ miniblog/src/pages/Register/Register.js
@@ -0,0 +1,59 @@
+import styles from "../Register/Register.module.css";
+
+import { useState, useEffect } from "react";
+
+const Register = () => {
+
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("")
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+    setError("")
+
+    const user = {
+      displayName, 
+      email,
+      password
+    }
+
+    if(password !== confirmPassword){
+      setError("As senhas precisam ser iguais!  ");
+      return
+    }
+    console.log(user)
+  }
+
+  return (
+    <div className={styles.register}>
+        <h1>Cadastre-se para postar</h1>
+        <p>Crie seu usuário e compartilhe suas histórias</p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">
+            <span>Nome:</span>
+            <input type="text" name="displayName" required placeholder="Nome do usuário" value={displayName} onChange={(e)=>setDisplayName(e.target.value)}/>
+          </label>
+          <label htmlFor="">
+            <span>E-mail:</span>
+            <input type="email" name="email" required placeholder="E-mail do usuário" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          </label>
+          <label htmlFor="">
+            <span>Senha:</span>
+            <input type="password" name="password" required placeholder="Insira sua senha" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          </label>
+          <label htmlFor="">
+            <span>Senha:</span>
+            <input type="password" name="confirmPassword" required placeholder="Confirme a sua senha" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} />
+          </label>
+          <button className="btn">Cadastrar</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+    </div>
+  )
+}
+
+export default Register;
